bc633c849ec64931dd93f08af89d5933ffedfe81
김호경 <jackjack0@naver.com>
2024-10-22 04:00:38 +0900

    CheckPlayTimeInputError 테스트 생성

--- __tests__/CheckPlayTimeInputError.test.js
+++ __tests__/CheckPlayTimeInputError.test.js
@@ -3,3 +3,11 @@ import { CheckPlayTimeInputError } from "../내부동작/CheckPlayTimeInputError
 test("게임회수를 입력할때 공백을 입력", () => {
   expect(CheckPlayTimeInputError(" 4")).toBe(false);
 });
+
+test("게임회수를 정수가 아닌걸 입력", () => {
+  expect(CheckPlayTimeInputError("0.3")).toBe(false);
+});
+
+test("게임회수를 음의정수 입력", () => {
+  expect(CheckPlayTimeInputError("-4")).toBe(false);
+});
