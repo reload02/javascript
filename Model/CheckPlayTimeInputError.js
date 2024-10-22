36ee26f049f792ef068f8cf36a66c54b7affa2a1
김호경 <jackjack0@naver.com>
2024-10-22 20:27:30 +0900

    테스트

--- Model/CheckPlayTimeInputError.js
+++ Model/CheckPlayTimeInputError.js
@@ -1,7 +1,7 @@
-export const checkPlayTimeInputError = (playTime) => {
-  if (playTime.includes(" ")) return false;
-  playTime = Number(playTime);
-  if (!Number.isInteger(playTime)) return false;
-  if (playTime < 1) return false;
-  return true;
-};
+// export const checkPlayTimeInputError = (playTime) => {
+//   if (playTime.includes(" ")) return false;
+//   playTime = Number(playTime);
+//   if (!Number.isInteger(playTime)) return false;
+//   if (playTime < 1) return false;
+//   return true;
+// };
--- Model/MoveCar.js
+++ Model/MoveCar.js
@@ -1,18 +1,18 @@
-import {
-  FORWARD_STANDARD,
-  MAX_RANDOM_NUMBER_FOR_FORWARD,
-  MIN_RANDOM_NUMBER_FOR_FORWARD,
-} from "./Constant/constant.js";
+// import {
+//   FORWARD_STANDARD,
+//   MAX_RANDOM_NUMBER_FOR_FORWARD,
+//   MIN_RANDOM_NUMBER_FOR_FORWARD,
+// } from "./Constant/constant.js";
 
-export const moveCar = (carsArray, RandomNumber) => {
-  carsArray.forEach((car) => {
-    if (
-      RandomNumber(
-        MIN_RANDOM_NUMBER_FOR_FORWARD,
-        MAX_RANDOM_NUMBER_FOR_FORWARD
-      ) > FORWARD_STANDARD
-    )
-      car.currentPosition++;
-  });
-  return carsArray;
-};
+// export const moveCar = (carsArray, RandomNumber) => {
+//   carsArray.forEach((car) => {
+//     if (
+//       RandomNumber(
+//         MIN_RANDOM_NUMBER_FOR_FORWARD,
+//         MAX_RANDOM_NUMBER_FOR_FORWARD
+//       ) > FORWARD_STANDARD
+//     )
+//       car.currentPosition++;
+//   });
+//   return carsArray;
+// };
