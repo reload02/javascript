95949030ab8c967bd13716f71fa1bc65b8f17e84
김호경 <jackjack0@naver.com>
2024-10-24 19:12:13 +0900

    fix : 임포트 안된것 임포트함

--- Model/promptUntilCorrect.js
+++ Model/promptUntilCorrect.js
@@ -1,4 +1,5 @@
 import { askQuestion } from "../View/InputView/askQuestion.js";
+import { printError } from "../View/outputView/printError.js";
 
 export const promptUntilCorrect = async (question, errorTypeToCheck) => {
   let value = null;
