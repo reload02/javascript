56e69d0fa73e54952c70c986a743d677b423ef39
김호경 <jackjack0@naver.com>
2024-10-26 19:15:43 +0900

    feat : 우승자 alert 메세지로 알려주기

--- Controller/PlayGame.js
+++ Controller/PlayGame.js
@@ -2,14 +2,16 @@ import { printWinner } from "../View/outputView/printWinner.js";
 import { printCarPosition } from "../View/outputView/printCarPosition.js";
 import { getWinner } from "../Model/getWInner.js";
 import { showCarPosition } from "../View/outputView/showCarposition.js";
+import { showWinner } from "../View/outputView/showWinner.js";
 
 const playGame = async (cars, playTime) => {
   cars = cars.split(",");
   let carsArray = cars.map((car) => ({ carName: car, currentPosition: 0 }));
   //printCarPosition(carsArray, playTime);
   await showCarPosition(carsArray, playTime);
-  let winners = getWinner(carsArray);
-  printWinner(winners);
+  let winners = await getWinner(carsArray);
+  //printWinner(winners);
+  await showWinner(winners);
 };
 
 export default playGame;
