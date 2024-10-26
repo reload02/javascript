9474319d73f3b2ca6cf5b4d833357f3f8755a00c
김호경 <jackjack0@naver.com>
2024-10-26 19:10:57 +0900

    fix : 게임 진행상황 도시 안정화

--- View/outputView/showCarposition.js
+++ View/outputView/showCarposition.js
@@ -4,20 +4,27 @@ import { randomNumber } from "../../Model/randomNumber.js";
 const CAR_PROGRESS_MARK = "■";
 
 export const showCarPosition = (carsArray, playTime) => {
-  let intervalId = setInterval(() => {
-    let currentstatus = `${playTime}회 남았습니다.\n`;
-    carsArray.forEach((car) => {
-      currentstatus =
-        currentstatus +
-        "\n" +
-        `${car.carName} : ${CAR_PROGRESS_MARK.repeat(car.currentPosition)}`;
-
-      document.body.innerHTML = `<pre style = "font-size: 16px; ">${currentstatus}</pre>`;
-    });
-    carsArray = moveCar(carsArray, randomNumber);
-    playTime--;
-    if (playTime < 0) {
-      clearInterval(intervalId);
-    }
-  }, 1000);
+  return new Promise((resolve) => {
+    let intervalId = setInterval(() => {
+      // console.log(carsArray[0].currentPosition);
+      // console.log(carsArray[1].currentPosition);
+      // console.log(carsArray[2].currentPosition); 질문용
+      //console.log(carsArray);
+      let currentstatus = `${playTime > 0 ? playTime : 0}회 남았습니다.\n`;
+      carsArray.forEach((car) => {
+        currentstatus =
+          currentstatus +
+          "\n" +
+          `${car.carName} : ${CAR_PROGRESS_MARK.repeat(car.currentPosition)}`;
+      });
+      carsArray = moveCar(carsArray, randomNumber);
+      document.body.innerHTML = `<pre>${currentstatus}</pre>`;
+      playTime--;
+      if (playTime < 0) {
+        clearInterval(intervalId);
+        document.body.innerHTML = `<pre>${currentstatus}</pre>`;
+        resolve();
+      }
+    }, 100);
+  });
 };
